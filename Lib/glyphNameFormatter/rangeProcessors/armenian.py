9fc7077d2efac45ac3ab342519f6f95340cd9742
Erik <erik@letterror.com>
2016-03-07 23:43:59 +0100

    Armenian

---
+++ Lib/glyphNameFormatter/rangeProcessors/armenian.py
@@ -0,0 +1,21 @@
+from glyphNameFormatter.scriptPrefixes import scriptPrefixes
+
+def process(self):
+    self.scriptTag = scriptPrefixes['armenian']
+    self.edit("ARMENIAN")
+
+    self.edit("APOSTROPHE", "apostrophe")
+    self.edit("EMPHASIS MARK", "emphasismark")
+    self.edit("QUESTION MARK", "question")
+    self.edit("EXCLAMATION MARK", "exclam")
+    self.edit("ABBREVIATION MARK", 'abbreviationmark')
+    self.edit("MODIFIER LETTER LEFT HALF RING", "ringhalfleft")
+    self.edit("SMALL LIGATURE ECH YIWN", "ech_yiwn")
+    self.edit("COMMA", "comma")
+    self.edit("FULL STOP", "period")
+    self.handleCase()
+    self.compress()
+
+if __name__ == "__main__":
+    from glyphNameFormatter.test import printRange
+    printRange("Armenian")
--- Lib/glyphNameFormatter/scriptPrefixes.py
+++ Lib/glyphNameFormatter/scriptPrefixes.py
@@ -1,12 +1,13 @@
 scriptPrefixes = {
-    'latin': "lt",
     'arabic': 'ar',
-    'ipa': 'ipa',
-    'greek': 'gr',
-    'hebrew': 'hb',
+    'armenian': 'am',
     'boxdrawings': 'bxd',
+    'CJK': 'cjk',
     'cyrillic': 'cy',
+    'greek': 'gr',
     'hangul': 'ko',
+    'hebrew': 'hb',
+    'ipa': 'ipa',
     'japan': 'jp',
-    'CJK': 'cjk'
+    'latin': "lt",
 }
