08a5f5f3aa38223df7e02893a0b6912970b00c03
Erik <erik@letterror.com>
2016-03-08 16:22:44 +0100

    CJK digit names. More numbers in Digit helper.

--- Lib/glyphNameFormatter/rangeProcessors/helper_digit_names.py
+++ Lib/glyphNameFormatter/rangeProcessors/helper_digit_names.py
@@ -1,7 +1,34 @@
 from glyphNameFormatter.scriptPrefixes import scriptPrefixes
 
 def process(self):
+
+    self.replace("JANUARY", "january")
+    self.replace("FEBRUARY", "february")
+    self.replace("MARCH", "march")
+    self.replace("APRIL", "april")
+    self.replace("MAY", "may")
+    self.replace("JUNE", "june")
+    self.replace("JULY", "july")
+    self.replace("AUGUST", "august")
+    self.replace("SEPTEMBER", "september")
+    self.replace("OCTOBER", "october")
+    self.replace("NOVEMBER", "november")
+    self.replace("DECEMBER", "december")
     
+    self.replace("THIRTY SIX", "thirtysix")
+    self.replace("THIRTY SEVEN", "thirtyseven")
+    self.replace("THIRTY EIGHT", "thirtyeight")
+    self.replace("THIRTY NINE", "thirtynine")
+    self.replace("FORTY ONE", "fortyone")
+    self.replace("FORTY TWO", "fortytwo")
+    self.replace("FORTY THREE", "fortythree")
+    self.replace("FORTY FOUR", "fortyfour")
+    self.replace("FORTY FIVE", "fortyfive")
+    self.replace("FORTY SIX", "fortysix")
+    self.replace("FORTY SEVEN", "fortyseven")
+    self.replace("FORTY EIGHT", "fortyeight")
+    self.replace("FORTY NINE", "fortynine")
+
     self.replace("FOURTEEN", "fourteen")
     self.replace("SIXTEEN", "sixteen")
     self.replace("SEVENTEEN", "seventeen")
@@ -24,6 +51,9 @@ def process(self):
     self.replace("THIRTEEN", "thirteen")
     self.replace("FIFTEEN", "fifteen")
     self.replace("TWENTY", "twenty")
+    self.replace("THIRTY", "thirty")
+    self.replace("FORTY", "forty")
+    self.replace("FIFTY", "fifty")
 
 if __name__ == "__main__":
     from glyphNameFormatter.test import printRange
