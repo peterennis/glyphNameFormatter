70428aa20a6f0bd8087c5881d8f8b1a6c4a0aa70
Erik <erik@letterror.com>
2016-03-08 13:19:39 +0100

    Hiragana

---
+++ Lib/glyphNameFormatter/rangeProcessors/hiragana.py
@@ -0,0 +1,18 @@
+from glyphNameFormatter.scriptPrefixes import scriptPrefixes
+
+def process(self):
+    self.scriptTag = scriptPrefixes['japan']
+
+    self.edit("LETTER")    
+    self.edit("SMALL", "small")
+    self.edit("KATAKANA-HIRAGANA VOICED SOUND MARK", "voicedmarkkana")
+    self.edit("KATAKANA-HIRAGANA SEMI-VOICED SOUND MARK", "semivoicedmarkkana")
+    self.edit("HIRAGANA ITERATION MARK", "iterationhiragana")
+    self.edit("HIRAGANA VOICED ITERATION MARK", "voicediterationhiragana")
+    self.editToFinal("HIRAGANA", "hiragana")
+    self.editToFinal("COMBINING", "cmb")
+    self.lower()
+
+if __name__ == "__main__":
+    from glyphNameFormatter.test import printRange
+    printRange("Hiragana")
