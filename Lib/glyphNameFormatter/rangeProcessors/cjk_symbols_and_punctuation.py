6fadf768936e2f02977111904063ce335658fc2f
Erik <erik@letterror.com>
2016-03-08 17:17:59 +0100

    CJK Letterlike symbols and punctuation

---
+++ Lib/glyphNameFormatter/rangeProcessors/cjk_symbols_and_punctuation.py
@@ -0,0 +1,20 @@
+from glyphNameFormatter.scriptPrefixes import scriptPrefixes
+
+def process(self):
+    self.edit("HANGZHOU NUMERAL", "hangzhou")
+
+    self.edit("JAPANESE INDUSTRIAL STANDARD SYMBOL", "jis")
+    self.edit("REVERSED DOUBLE PRIME QUOTATION MARK", "quotedblprimereversed")
+    self.edit("DOUBLE PRIME QUOTATION MARK", "quotedblprime")
+    self.edit("REVERSED", "reversed")
+    self.edit("DOUBLE ANGLE", "dblangle")
+    self.edit("DOUBLE", "dbl")
+    self.edit("LEFT", "left")
+    self.edit("RIGHT", "right")
+    self.processAs("Helper Digit Names")
+    self.lower()
+    self.compress()
+
+if __name__ == "__main__":
+    from glyphNameFormatter.test import printRange
+    printRange("CJK Symbols and Punctuation")
