988cd24d5b01a1f5cc29e577c4746b8901ec00c6
Erik <erik@letterror.com>
2016-03-08 15:29:44 +0100

    Enclosed Alphanumerics, some more digit names

---
+++ Lib/glyphNameFormatter/rangeProcessors/enclosed_alphanumerics.py
@@ -0,0 +1,18 @@
+from glyphNameFormatter.scriptPrefixes import scriptPrefixes
+
+def process(self):
+    self.processAs("Helper Digit Names")
+    self.edit("DOUBLE CIRCLED", "circledbl")
+    self.edit("CIRCLED", "circle")
+    self.edit("DIGIT")
+    self.edit("LATIN")
+    self.edit("NUMBER")
+    self.edit("PARENTHESIZED", "parenthesized")
+    self.edit("FULL STOP", "period")
+    self.edit("NEGATIVE", "black")
+    self.handleCase()
+    self.compress()
+
+if __name__ == "__main__":
+    from glyphNameFormatter.test import printRange
+    printRange("Enclosed Alphanumerics")
--- Lib/glyphNameFormatter/rangeProcessors/helper_digit_names.py
+++ Lib/glyphNameFormatter/rangeProcessors/helper_digit_names.py
@@ -2,6 +2,12 @@ from glyphNameFormatter.scriptPrefixes import scriptPrefixes
 
 def process(self):
     
+    self.replace("FOURTEEN", "fourteen")
+    self.replace("SIXTEEN", "sixteen")
+    self.replace("SEVENTEEN", "seventeen")
+    self.replace("EIGHTEEN", "eighteen")
+    self.replace("NINETEEN", "nineteen")
+
     self.replace('ZERO', 'zero')
     self.replace('ONE', 'one')
     self.replace('TWO', 'two')
@@ -12,6 +18,12 @@ def process(self):
     self.replace('SEVEN', 'seven')
     self.replace('EIGHT', 'eight')
     self.replace('NINE', 'nine')
+    self.replace("TEN", "ten")
+    self.replace("ELEVEN", "eleven")
+    self.replace("TWELVE", "twelve")
+    self.replace("THIRTEEN", "thirteen")
+    self.replace("FIFTEEN", "fifteen")
+    self.replace("TWENTY", "twenty")
 
 if __name__ == "__main__":
     from glyphNameFormatter.test import printRange
