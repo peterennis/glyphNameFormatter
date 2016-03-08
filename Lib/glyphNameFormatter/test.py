ad761690aefdbe2e3dbf255d5c310293295e5b88
Frederik Berlaen <frederik@typemytype.com>
2016-03-08 10:50:14 +0100

    adding simple test for doubles

--- Lib/glyphNameFormatter/test.py
+++ Lib/glyphNameFormatter/test.py
@@ -1,11 +1,17 @@
+from glyphNameFormatter.unicodeRangeNames import getRangeByName
+from glyphNameFormatter import GlyphName
+
+from glyphNameFormatter.data import unicode2name_AGD
+
+
+def _rangeNameToRange(rangeName):
+    if isinstance(rangeName, tuple):
+        return rangeName
+    return getRangeByName(rangeName)
+
 
 def printRange(rangeName):
-    from glyphNameFormatter import GlyphName
-    from glyphNameFormatter.unicodeRangeNames import getRangeByName
-
-    from glyphNameFormatter.data import unicode2name_AGD
-
-    for u in range(*getRangeByName(rangeName)):
+    for u in range(*_rangeNameToRange(rangeName)):
         g = GlyphName(uniNumber=u)
         name = g.getName()
         if name is None:
@@ -15,3 +21,30 @@ def printRange(rangeName):
             AGDName = ""
 
         print name.ljust(50), AGDName.ljust(30), "%04X" % g.uniNumber, "\t", g.uniLetter.encode("utf-8"), "\t", g.uniName
+
+    testDoubles(rangeName)
+
+
+def testDoubles(rangeName):
+    """
+    test if there are doubles
+    """
+    names = set()
+    doubles = set()
+    for u in range(*_rangeNameToRange(rangeName)):
+        g = GlyphName(uniNumber=u)
+        name = g.getName()
+        if name in names:
+            doubles.add(name)
+        else:
+            names.add(name)
+    if doubles:
+        print "doulbes: %s" % ", ".join(sorted(doubles))
+
+
+def testGLIFFileName(rangeName):
+    """
+    test on glif file name
+    """
+
+
