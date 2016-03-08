8ddc0264a2b4e8760751f4ea3441f44f017283b0
Frederik Berlaen <frederik@typemytype.com>
2016-03-08 23:49:34 +0100

    work in progress

--- Lib/glyphNameFormatter/test/export.py
+++ Lib/glyphNameFormatter/test/export.py
@@ -1,21 +1,32 @@
 from glyphNameFormatter import GlyphName
-from glyphNameFormatter.unicodeRangeNames import getAllRangeNames, getRangeByName
 
-data = [
-    "# format",
-    "# <glyphName> <hex unicode>"
-]
+from glyphNameFormatter.unicodeRangeNames import getAllRangeNames, getRangeByName, rangeNameToModuleName
 
-for rangeName in getAllRangeNames():
-    for u in range(*getRangeByName(rangeName)):
-        g = GlyphName(uniNumber=u)
-        name = g.getName()
-        if name is None:
-            continue
-        data.append("%s %04X" % (name, u))
+import importlib
 
-path = "./../names/glyphNamesToUnicode"
 
-f = open(path, "w")
-f.write("\n".join(data))
-f.close()
+def generateFlat(path, onlySupported=True):
+    data = [
+        "# format",
+        "# <glyphName> <hex unicode>"
+    ]
+    for rangeName in getAllRangeNames():
+        if onlySupported:
+            moduleName = rangeNameToModuleName(rangeName)
+            try:
+                module = importlib.import_module('glyphNameFormatter.rangeProcessors.%s' % moduleName)
+            except:
+                continue
+
+        for u in range(*getRangeByName(rangeName)):
+            g = GlyphName(uniNumber=u)
+            name = g.getName()
+            if name is None:
+                continue
+            data.append("%s %04X" % (name, u))
+
+    f = open(path, "w")
+    f.write("\n".join(data))
+    f.close()
+
+generateFlat(path="./../names/glyphNamesToUnicode")
