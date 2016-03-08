bb630333056b0ecd8453650a63ab71055d1df8b1
Frederik Berlaen <frederik@typemytype.com>
2016-03-08 22:17:03 +0100

    read unicode blocks from unicode.org source

--- Lib/glyphNameFormatter/data/__init__.py
+++ Lib/glyphNameFormatter/data/__init__.py
@@ -2,7 +2,10 @@ import os
 
 path = os.path.dirname(__file__)
 
-# AGD
+# =======
+# = AGD =
+# =======
+
 unicode2name_AGD = {}
 name2unicode_AGD = {}
 
@@ -10,9 +13,40 @@ f = open(os.path.join(path, "AGD_name_uni.txt"), "r")
 lines = f.readlines()
 f.close()
 
+# format
+# glyphName hexUnicode
 for line in lines:
     if line:
         name, uni = line.split()
         uni = int(uni, 16)
         unicode2name_AGD[uni] = name
         name2unicode_AGD[name] = uni
+
+
+# ==================
+# = unicode blocks =
+# ==================
+
+unicodeRangeNames = {}
+
+f = open(os.path.join(path, "unicodeBlocks.txt"))
+lines = f.readlines()
+f.close()
+
+# format
+# Start Code..End Code; Block Name
+for line in lines:
+    if line.startswith("#"):
+        # a comment
+        continue
+    line = line.strip()
+    if not line:
+        # empty line
+        continue
+    rangeValues, rangeName = line.split(";")
+    rangeName = rangeName.strip()
+    start, end = rangeValues.split("..")
+    start = int(start, 16)
+    end = int(end, 16)
+
+    unicodeRangeNames[(start, end)] = rangeName
