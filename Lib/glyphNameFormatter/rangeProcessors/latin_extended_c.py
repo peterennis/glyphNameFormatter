658cc571ab59363c49122c21fa6fe4cc9141952d
Frederik Berlaen <frederik@typemytype.com>
2016-03-09 16:03:03 +0100

    latin ext c

---
+++ Lib/glyphNameFormatter/rangeProcessors/latin_extended_c.py
@@ -0,0 +1,29 @@
+from glyphNameFormatter.data.scriptPrefixes import addScriptPrefix
+
+
+def process(self):
+    # edits go here
+    self.edit("LATIN")
+
+    self.replace("CAPITAL LETTER ALPHA", addScriptPrefix("Alpha", script="latin"))
+    self.replace("CAPITAL LETTER TURNED ALPHA", addScriptPrefix("Alphaturned", script="latin"))
+
+    self.replace("LETTER SMALL CAPITAL TURNED E", "Esmallturned")
+
+    self.edit("DOUBLE", "dbl")
+    self.edit("BAR", "bar")
+    self.edit("SUBSCRIPT", ".inferior")
+    self.edit("HALF", "half")
+    self.edit("TAILLESS", "tailless")
+
+    self.edit("DIAGONAL")
+    self.edit("WITH")
+
+    self.processDiacritics()
+    self.processShape()
+    self.handleCase()
+    self.compress()
+
+if __name__ == "__main__":
+    from glyphNameFormatter.test import printRange
+    printRange("Latin Extended-C")
