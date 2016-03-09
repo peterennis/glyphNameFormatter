2e3f43e857d83ae217785351217de6616935db02
Erik <erik@letterror.com>
2016-03-09 22:16:16 +0100

    Some Indic ranges with a shared helper.

--- Lib/glyphNameFormatter/rangeProcessors/gujarati.py
+++ Lib/glyphNameFormatter/rangeProcessors/gujarati.py
@@ -1,12 +1,20 @@
 
 def process(self):
-    self.edit("ORIYA")
+
+	#GUJARATI VOWEL SIGN CANDRA E
+	#GUJARATI VOWEL CANDRA E
+
+    self.edit("GUJARATI")
     self.edit("LETTER")
     self.edit("DIGIT")
+    self.edit("VOWEL SIGN", "sign")
+    self.edit("VOWEL")
+    self.edit("SIGN")
     self.processAs("Helper Indic")
     self.processAs("Helper Numbers")
     self.lower()
+    
 
 if __name__ == "__main__":
     from glyphNameFormatter.test import printRange
-    printRange("Oriya")
+    printRange("Gujarati")
