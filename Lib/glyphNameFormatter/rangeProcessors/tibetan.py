b10ca17ab10fe541fea56263ebd19aff4f857ed5
Frederik Berlaen <frederik@typemytype.com>
2016-03-09 14:19:36 +0100

    updating tibetan

--- Lib/glyphNameFormatter/rangeProcessors/tibetan.py
+++ Lib/glyphNameFormatter/rangeProcessors/tibetan.py
@@ -2,29 +2,34 @@
 def process(self):
     self.edit("TIBETAN")
     if self.has("DIGIT"):
-        self.edit("DIGIT", "")
+        self.edit("DIGIT")
         self.lower()
-
-    # self.edit("SIGN", 'sign')
-    # self.edit("MARK", 'mark')
-    # self.replace("VOWEL", "vowel")
-    # self.edit("SYMBOL", "symbol")
-    # self.edit("SUBJOINED", "subjoined")
-    # self.replace("LOGOTYPE", "logotype")
-    # self.edit("FIXED-FORM", "fixed")
-
-    # self.replace("INITIAL", 'initial')
-    # self.replace("CLOSING", 'closing')
-    # self.replace("CANTILLATION", "cantillation")
-    # self.edit("VOCALIC", "vocalic")
 
     self.edit("RIGHT-FACING SVASTI SIGN WITH DOTS", "svastirightdot")
     self.edit("LEFT-FACING SVASTI SIGN WITH DOTS", "svastileftdot")
     self.edit("RIGHT-FACING SVASTI SIGN", "svastiright")
     self.edit("LEFT-FACING SVASTI SIGN", "svastileft")
 
+    self.edit("VOCALIC", "vocalic")
+    self.edit("SYLLABLE", "syllable")
+    self.edit("VOWEL", "vowel")
+    self.edit("LOGOTYPE", "logotype")
+    self.edit("INITIAL", 'initial')
+    self.edit("CANTILLATION", "cantillation")
+    self.edit("CLOSING", "closing")
+    self.edit("SYMBOL", "symbol")
+    self.edit("SUBJOINED", "subjoined")
+    self.edit("FIXED-FORM", "fixed")
+    self.edit("SIGN", 'sign')
+    self.edit("MARK", 'mark')
+    self.edit("LEFT", "left")
+    self.edit("RIGHT", "right")
+
     self.edit("-A", "AA")  # Old name TIBETAN LETTER AA
+    self.edit("-")
     self.edit("LETTER")
+
+    self.lower()
     self.compress()
 
 if __name__ == "__main__":
