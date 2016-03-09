08e471a81cff2f6d0bbc3e373c922d0387cb0065
Frederik Berlaen <frederik@typemytype.com>
2016-03-09 14:19:42 +0100

    updating mongolian

--- Lib/glyphNameFormatter/rangeProcessors/mongolian.py
+++ Lib/glyphNameFormatter/rangeProcessors/mongolian.py
@@ -1,11 +1,22 @@
 
 def process(self):
-
     # edits go here
     self.edit("MONGOLIAN")
+    self.edit("DIGIT")
 
-    self.handleCase()
+    self.replace("FULL STOP", "period")
+
+    parts = [
+        "TODO", "SIBE", "MANCHU", "ALI GALI", "HALF", "THREE",
+
+        "INVERTED",
+        ]
+    for part in parts:
+        self.edit(part, part.lower().replace(" ", ""))
+
     self.replace("LETTER")
+
+    self.lower()
     self.compress()
 
 if __name__ == "__main__":
