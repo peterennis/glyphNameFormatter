56a61af5ecf961412ea3f54ee91113d602450b4a
Frederik Berlaen <frederik@typemytype.com>
2016-03-11 10:32:44 +0100

    add prefix for hangul

--- Lib/glyphNameFormatter/rangeProcessors/hangul_jamo.py
+++ Lib/glyphNameFormatter/rangeProcessors/hangul_jamo.py
@@ -8,6 +8,7 @@ def process(self):
     self.replace("o-e", "o_e")  # ?
     self.replace("-")
     self.compress()
+    self.scriptPrefix()
 
 if __name__ == "__main__":
     from glyphNameFormatter.test import printRange
