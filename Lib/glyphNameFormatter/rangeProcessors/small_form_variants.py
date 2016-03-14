9908c645691fc9e5f8b01961cbf606dfd8487cbb
Frederik Berlaen <frederik@typemytype.com>
2016-03-14 12:29:02 +0100

    add scriptTag to solve doubles conflict

--- Lib/glyphNameFormatter/rangeProcessors/small_form_variants.py
+++ Lib/glyphNameFormatter/rangeProcessors/small_form_variants.py
@@ -1,9 +1,12 @@
+from glyphNameFormatter.data.scriptPrefixes import scriptPrefixes
+
 
 def process(self):
     self.edit("SMALL")
     self.edit("IDEOGRAPHIC", "ideographic")
     self.processAs("Basic Latin")
     self.processAs("General Punctuation")
+    self.scriptTag = scriptPrefixes["Small Form Variants"]
 
 if __name__ == "__main__":
     from glyphNameFormatter.test import printRange
