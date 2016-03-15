65e0bad0ab7fef4f784d836f541c7a3e98a56d0a
Frederik Berlaen <frederik@typemytype.com>
2016-03-15 23:11:42 +0100

    emoticons!

---
+++ Lib/glyphNameFormatter/rangeProcessors/emoticons.py
@@ -0,0 +1,10 @@
+
+
+def process(self):
+    # just to test unicode 8 stuff
+    # not complete
+    self.compress()
+
+if __name__ == "__main__":
+    from glyphNameFormatter.exporters import printRange
+    printRange("Emoticons")
