5af14f23f99c1f9d934dc79aefd5cb610175ad9f
Frederik Berlaen <frederik@typemytype.com>
2016-04-25 15:08:49 +0200

    ignore of data files doesnt exists
    helpful, so not all data files are needed to be embedded

--- Lib/glyphNameFormatter/data/__init__.py
+++ Lib/glyphNameFormatter/data/__init__.py
@@ -20,26 +20,29 @@ path = os.path.dirname(__file__)
 unicode2name_AGD = {}
 name2unicode_AGD = {}
 
-f = open(os.path.join(path, "AGD.txt"), "r")
-lines = f.readlines()
-f.close()
+AGDPath = os.path.join(path, "AGD.txt")
 
-# format
-# <glyphName>
-# <tab> <tag>: <value>
+if os.path.exists(AGDPath):
+    f = open(AGDPath, "r")
+    lines = f.readlines()
+    f.close()
 
-currentGlyphName = None
-for line in lines:
-    if line.startswith("\t") and currentGlyphName is not None:
-        tag, value = line.split(":")
-        tag = tag.strip()
-        value = value.strip()
-        if tag == "uni":
-            value = int(value, 16)
-            unicode2name_AGD[value] = currentGlyphName
-            name2unicode_AGD[currentGlyphName] = value
-    else:
-        currentGlyphName = line.strip()
+    # format
+    # <glyphName>
+    # <tab> <tag>: <value>
+
+    currentGlyphName = None
+    for line in lines:
+        if line.startswith("\t") and currentGlyphName is not None:
+            tag, value = line.split(":")
+            tag = tag.strip()
+            value = value.strip()
+            if tag == "uni":
+                value = int(value, 16)
+                unicode2name_AGD[value] = currentGlyphName
+                name2unicode_AGD[currentGlyphName] = value
+        else:
+            currentGlyphName = line.strip()
 
 
 # ==========================
@@ -51,17 +54,20 @@ for line in lines:
 unicode2name_GLYPHS = {}
 name2unicode_GLYPHS = {}
 
-tree = ET.parse(os.path.join(path, "GlyphData.xml"))
+glyphDataPath = os.path.join(path, "GlyphData.xml")
 
-for i in tree.iter():
-    if i.tag == "glyph":
-        u = i.attrib.get("unicode")
-        if u:
-            u = int(u, 16)
-            n = i.attrib.get("name")
-            n = n.split("-")[0]
-            unicode2name_GLYPHS[u] = n
-            name2unicode_GLYPHS[n] = u
+if os.path.exists(glyphDataPath):
+    tree = ET.parse(glyphDataPath)
+
+    for i in tree.iter():
+        if i.tag == "glyph":
+            u = i.attrib.get("unicode")
+            if u:
+                u = int(u, 16)
+                n = i.attrib.get("name")
+                n = n.split("-")[0]
+                unicode2name_GLYPHS[u] = n
+                name2unicode_GLYPHS[n] = u
 
 # ================
 # = unicode list =
@@ -69,25 +75,28 @@ for i in tree.iter():
 
 unicodelist = {}
 
-f = open(os.path.join(path, "flatUnicode.txt"))
-lines = f.readlines()
-f.close()
+flatUnicodePath = os.path.join(path, "flatUnicode.txt")
 
-unicodeVersion = lines[0].replace("#", "").strip()
+if os.path.exists(flatUnicodePath):
+    f = open(flatUnicodePath, "r")
+    lines = f.readlines()
+    f.close()
 
-for line in lines:
-    if line.startswith("#"):
-        # a comment
-        continue
-    line = line.strip()
-    if not line:
-        # empty line
-        continue
-    uniNumber, uniName = line.split("\t")
-    uniNumber = int(uniNumber, 16)
-    unicodelist[uniNumber] = uniName
+    unicodeVersion = lines[0].replace("#", "").strip()
 
-unicodeVersion = lines[0].replace("#", "").strip()
+    for line in lines:
+        if line.startswith("#"):
+            # a comment
+            continue
+        line = line.strip()
+        if not line:
+            # empty line
+            continue
+        uniNumber, uniName = line.split("\t")
+        uniNumber = int(uniNumber, 16)
+        unicodelist[uniNumber] = uniName
+
+    unicodeVersion = lines[0].replace("#", "").strip()
 
 # ==================
 # = unicode blocks =
@@ -95,24 +104,27 @@ unicodeVersion = lines[0].replace("#", "").strip()
 
 unicodeRangeNames = {}
 
-f = open(os.path.join(path, "unicodeBlocks.txt"))
-lines = f.readlines()
-f.close()
+unicodeBlocksPath = os.path.join(path, "unicodeBlocks.txt")
 
-# format
-# Start Code..End Code; Block Name
-for line in lines:
-    if line.startswith("#"):
-        # a comment
-        continue
-    line = line.strip()
-    if not line:
-        # empty line
-        continue
-    rangeValues, rangeName = line.split(";")
-    rangeName = rangeName.strip()
-    start, end = rangeValues.split("..")
-    start = int(start, 16)
-    end = int(end, 16)
+if os.path.exists(unicodeBlocksPath):
+    f = open(unicodeBlocksPath, "r")
+    lines = f.readlines()
+    f.close()
 
-    unicodeRangeNames[(start, end)] = rangeName
+    # format
+    # Start Code..End Code; Block Name
+    for line in lines:
+        if line.startswith("#"):
+            # a comment
+            continue
+        line = line.strip()
+        if not line:
+            # empty line
+            continue
+        rangeValues, rangeName = line.split(";")
+        rangeName = rangeName.strip()
+        start, end = rangeValues.split("..")
+        start = int(start, 16)
+        end = int(end, 16)
+
+        unicodeRangeNames[(start, end)] = rangeName
