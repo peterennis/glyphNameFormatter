ff1c7d267f5433b3f2abefbd567fb25ac7127360
Denis Moyogo Jacquerye <moyogo@gmail.com>
2016-11-08 10:17:16 +0000

    buildFlatUnicodeList: download or specify which ucd.all.flat.xml to use

--- Lib/glyphNameFormatter/data/buildFlatUnicodeList.py
+++ Lib/glyphNameFormatter/data/buildFlatUnicodeList.py
@@ -1,4 +1,16 @@
-"""
+from __future__ import print_function
+import os
+import sys
+import tempfile
+from zipfile import ZipFile
+from xml.etree import cElementTree as ET
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
+
+__doc__ = """
 This will parse the large ucd xml into a simple list that is workable and
 is fair enough for download and embeding.
 
@@ -6,17 +18,50 @@ starts with the # unicode description/version
 
 format
 <unicode> \t <unicode name>
-
-download from
-http://www.unicode.org/Public/8.0.0/ucdxml/ucd.all.flat.zip
 """
 
-import os
-from xml.etree import cElementTree as ET
+URL = "http://www.unicode.org/Public/{version}/ucdxml/ucd.all.flat.zip"
+UNICODE_VERSION = "9.0.0"
+UCD_ZIP_FILE = "ucd.all.flat.zip"
+UCD_FILE = UCD_ZIP_FILE[:-3] + "xml"
+FLAT_FILE = "flatUnicode.txt"
 
-# after download it will be in the download folder :)
-path = os.path.expanduser("~/Downloads/ucd.all.flat.xml")
+parser = ArgumentParser(description=__doc__,
+                        formatter_class=RawDescriptionHelpFormatter)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-u", "--unicode-version",
+                   help="Unicode version to use for download and processing")
+group.add_argument("-i", "--input", metavar="UCD_FILE",
+                   help="Path to copy of {ucd_file} for processing. "
+                   "Use if you already have an up-to-date local copy of "
+                   "{ucd_file}.".format(ucd_file=UCD_FILE))
+options = parser.parse_args()
 
+if options.input:
+    path = options.input
+else:
+    tempdir = tempfile.mkdtemp()
+    filename = os.path.join(tempdir, UCD_ZIP_FILE)
+    print(">> Downloading {} to {}".format(UCD_ZIP_FILE, filename))
+    if options.unicode_version:
+        version = options.unicode_version
+    else:
+        version = UNICODE_VERSION
+    url = urlopen(URL.format(version=version))
+    with open(filename, "wb") as fp:
+        blocksize = 8192
+        while True:
+            buffer = url.read(blocksize)
+            if not buffer:
+                break
+            fp.write(buffer)
+    zipped = ZipFile(filename)
+    print(">> Extracting {} to {}"
+          .format(UCD_FILE, os.path.join(tempdir, UCD_FILE)))
+    path = zipped.extract(UCD_FILE, tempdir)
+
+
+print(">> Building {} from {}".format(FLAT_FILE, path))
 tree = ET.parse(path)
 
 flat = []
@@ -29,6 +74,6 @@ for i in tree.iter():
             flat.append("%s\t%s" % (i.attrib.get("cp"), n))
 
 
-f = open("flatUnicode.txt", "w")
+f = open(FLAT_FILE, "w")
 f.write("\n".join(flat))
 f.close()
