19f216e8ab331d3bac74721f34ab11723be765b5
Erik van Blokland <erik@letterror.com>
2017-10-03 23:27:19 +0200

    Improvements in the Misc symbols and pictographs.

--- Lib/glyphNameFormatter/rangeProcessors/miscellaneous_symbols_and_pictographs.py
+++ Lib/glyphNameFormatter/rangeProcessors/miscellaneous_symbols_and_pictographs.py
@@ -2,8 +2,20 @@
 def process(self):
     #self.setExperimental()
     #self.edit("circled", )
-    self.edit("-", " ")
+    self.replace("EUROPE-AFRICA", "Europe Africa")
+    self.replace("ASIA-AUSTRALIA", "Asia Australia")
     self.edit("SYMBOL", "")
+    self.replace("FORK AND KNIFE", "fork knife")
+    self.replace("WITH PLATE", "plate")
+    self.replace("SNOW CAPPED", "snowcapped")
+    self.edit("WITH", "")
+    self.replace("JACK-O-LANTERN", "jack O Lantern")
+    self.edit("UP-POINTING", "Up")
+    self.edit("UP-POINTING", "Up")
+    self.edit("DOWN-POINTING", "DOwn")
+    self.replace("PERFORMING", "Performing")
+
+    self.edit("FOR", "")
     self.camelCase()
 
 if __name__ == "__main__":
