78f9e091ae9bae241832b509cf8f9b7de0ce27ba
Erik van Blokland <erik@letterror.com>
2019-06-26 17:29:10 +0200

    Work on PR #82 Clarify characters narmodGeor, hardsignGeor, labialsignGeor

--- Lib/glyphNameFormatter/rangeProcessors/georgian.py
+++ Lib/glyphNameFormatter/rangeProcessors/georgian.py
@@ -2,10 +2,13 @@
 def process(self):
     # edits go here
     self.edit("GEORGIAN PARAGRAPH SEPARATOR", "paragraphseparator")
-    self.edit("MODIFIER LETTER GEORGIAN NAR", "narmod")
+    self.edit("MODIFIER LETTER GEORGIAN NAR", "narmodGeor")
+    self.edit("GEORGIAN LETTER HARD SIGN", "hardsignGeor")
+    self.edit("GEORGIAN LETTER LABIAL SIGN", "labialsignGeor")
+
     self.edit("GEORGIAN")
     self.handleCase()
-    if self.has("GEORGIAN LETTER"):
+    if self.has("GEORGIAN LETTER") and  self.uniNumber not in [0x10FC, 0x10FE, 0x10FF]:
         self.edit("LETTER")
         self.suffix("Mkhedruli")
         self.lower()
