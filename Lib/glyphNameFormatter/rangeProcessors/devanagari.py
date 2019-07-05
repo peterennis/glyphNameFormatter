68e8791bf4452794ca4ff61c7e71d96edb17eb9c
Erik van Blokland <erik@letterror.com>
2019-07-05 11:44:06 +0200

    WIP #83 fix esigncandralong add suffixen column to tools.debug() regenerate the names.

--- Lib/glyphNameFormatter/rangeProcessors/devanagari.py
+++ Lib/glyphNameFormatter/rangeProcessors/devanagari.py
@@ -2,21 +2,19 @@
 def process(self):
     self.edit("DEVANAGARI")
 
-    self.edit("LONG E", "longe")
     # parts
     self.edit("LETTER")
     self.edit("DIGIT")
-
-    #self.edit("VOWEL SIGN VOCALIC", "vocalsign")
     self.edit("VOWEL SIGN", "sign")
-    #self.edit("VOCALIC", "vocal")
-
-    self.edit("CANDRA LONG", "candralong")
-    self.edit("SHORT", "short")
-    self.edit("DOUBLE", "dbl")
 
     self.edit("SIGN CANDRABINDU", "candrabindu")
     self.edit("SIGN INVERTED CANDRABINDU", "candrabinduinverted")
+
+    self.edit("CANDRA", "candra")
+    self.edit("DOUBLE", "dbl")
+    self.edit("SHORT", "short")
+    self.edit("LONG", "long")
+
 
     self.processAs("Helper Indic")
 
@@ -43,3 +41,5 @@ if __name__ == "__main__":
     printRange("Devanagari")
     from glyphNameFormatter.tools import debug
     debug(0x0955)
+    debug(0x0900)    
+    debug(0x0901)        
